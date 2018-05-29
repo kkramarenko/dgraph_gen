9803e74b61df16af000b1cd5ef2bdd7ff3f9cab6
Konstantin Kramarenko <kostya.kram@gmail.com>
2018-05-29 11:14:07 +0700

    Generator of syndromes added

--- digraph.py
+++ digraph.py
@@ -1,9 +1,84 @@
 import numpy as np
 
-def graph_gen(N):
-    
-    print N
+def syndrome_gen(N, N_faulty):
+    """Function for generating syndrome of system.
 
+    Args:
+        N: Total number of nodes of computer system.
+        N_faulty: Number of faulty nodes.
+
+    Returns:
+        Set of fafulty_nodes and syndrome corresponded to system state.    
+    """
     
-    return 0
+#    print N
+    syndrome = np.empty((N, N))
+
+    syndrome[:] = -1
+
+#    tmp = np.reshape(syndrome, N*N)
+#    print syndrome
+#    print tmp
+
+    N_faulty = np.random.randint(low=1, high=N_faulty+1)
+#   Selecting randomly nodes which ones will be faulty
+    faulty_set = set()
+    while len(faulty_set) < N_faulty:
+        tmp = np.random.random_integers(low=1, high=N, size=N_faulty)
+        faulty_set.update(tmp)
+#        print faulty_set
+#        print len(faulty_set)
+#        print tmp
+    
+    tmp = np.array(list(faulty_set)[:N_faulty]) - 1
+    faulty_set = set(tmp)
+#    print faulty_set
+#    print faulty_nodes
+#    print len(faulty_nodes)
+
+#   Generate syndrome     
+    for idx1 in range(N):
+        for idx2 in range(N):
+            if idx1 != idx2:
+                if idx1 in faulty_set:
+                    syndrome[idx1][idx2] = np.random.randint(low=0, high=2)
+                else:    
+                    if idx2 in faulty_set:
+                        syndrome[idx1][idx2] = 1
+                    else:
+                        syndrome[idx1][idx2] = 0
+
+    faulty_nodes = np.zeros(N)
+    for idx in range(N):
+        if idx in faulty_set:
+            faulty_nodes[idx] = 1
+    
+    syndrome = syndrome.reshape(-1, )
+    return faulty_nodes, syndrome
+
+def trainset_gen(length, N, N_faulty):
+    """Function for generating trainset.
+
+    Args:
+        Length: Total length of training set
+        N: Total number of nodes of computer system.
+        N_faulty: Number of faulty nodes.
+
+    Returns:
+        Training inputs and outputs.    
+    """
+
+    Y = np.zeros(N)
+    X = np.zeros(N*N)
+#    print Y.shape
+#    print X.shape
+    while length > 0:
+        tmp_Y, tmp_X = syndrome_gen(N, N_faulty)
+        Y = np.vstack([Y, tmp_Y])
+        X = np.vstack([X, tmp_X])
+        length -= 1
+
+    return Y, X
+
+#trainset_gen(1, 5, 2)
 
--- test.py
+++ test.py
@@ -1,33 +1,55 @@
 import numpy as np
-from digraph import graph_gen
+#import graphviz
+from time import time
+#from datetime import timedelta
+from digraph import trainset_gen
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.preprocessing import MultiLabelBinarizer
+#from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.metrics import label_ranking_average_precision_score
+#from sklearn.tree import export_graphviz
 
-X = [[1, 0, 0, 0, 1],
-     [0, 0, 0, 1, 0],
-     [0, 1, 0, 1, 0]]
+#X = [[1, 0, 0, 0, 1],
+#     [0, 0, 0, 1, 0],
+#     [0, 1, 0, 1, 0]]
 
-Y = [[1, 3], [2], [1]]
+#Y1 = [[1, 3], [2], [1]]
+N = 100
+N_faulty = 10
+trainset_length = 1000
+n_estim = 1000
 
-print X
-print Y
+print "N: ", N
+print "N_faulty: ", N_faulty
+print "Length of trainset: ", trainset_length
+print "n_estimators: ", n_estim
 
-mlb = MultiLabelBinarizer()
-Y = mlb.fit_transform(Y)
+time_start = time()
+Y, X = trainset_gen(trainset_length, N, N_faulty)
+time_end = time()
+print "Time of generating trainset: ", time_end - time_start, "seconds!" 
 
-print Y
+train_len = int(trainset_length * 0.7)
+X_train = X[:train_len]
+X_test = X[train_len+1:]
+Y_train = Y[:train_len]
+Y_test = Y[train_len+1:]
 
-model = RandomForestClassifier()
-model.fit(X, Y)
+time_start = time()
+model = RandomForestClassifier(n_estimators=n_estim)
+model.fit(X_train, Y_train)
+time_end =  time()
+print "Time of learning: ", time_end - time_start, "seconds!"
 
-ret = model.predict(X)
-print ret
+ret = model.predict(X_test)
+#print ret
 
-scores = model.score(X, Y)
-print scores
+#scores = model.score(X, Y)
+#print scores
 
-scores = label_ranking_average_precision_score(Y, ret)
-print scores
-#graph_gen(51)
+scores = label_ranking_average_precision_score(Y_test, ret)
+print "Test set score: ", scores
+
+ret = model.predict(X_train)
+scores = label_ranking_average_precision_score(Y_train, ret)
+print "Train set score: ", scores
 
